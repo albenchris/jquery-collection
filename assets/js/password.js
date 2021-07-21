b0c21e1f6e990e84cc7631a3aabb1977877697c6
albenchris <albenchris00@gmail.com>
2021-07-21 15:02:54 -0500

    refactored getRandomSymbol to include more symbols

--- assets/js/password.js
+++ assets/js/password.js
@@ -5,7 +5,10 @@
 const getRandomLowercase = () => String.fromCharCode(Math.floor(Math.random() * 26 + 97));
 const getRandomUppercase = () => String.fromCharCode(Math.floor(Math.random() * 26 + 65));
 const getRandomNumber = () => JSON.stringify(Math.floor(Math.random() * 10));
-const getRandomSymbol = () => String.fromCharCode(Math.floor(Math.random() * 6 + 33));
+const getRandomSymbol = () => {
+    const symbols = '!@#$%^&*()-_=+[{]}<>?';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
 console.log(getRandomLowercase());
 console.log(getRandomUppercase());
