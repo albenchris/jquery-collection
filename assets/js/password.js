a24e070e549b88c464918be880466f25de62fd4a
albenchris <albenchris00@gmail.com>
2021-07-21 16:32:21 -0500

    new event listener for generate password button

--- assets/js/password.js
+++ assets/js/password.js
@@ -1,17 +1,32 @@
 
+$('#generate').on('click', () => {
+    const length = +$('#length').val(); // + in place of parseInt()
+    const upper = $('#uppercase')[0].checked;
+    const lower = $('#lowercase')[0].checked;
+    const numbers = $('#numbers')[0].checked;
+    const symbols = $('#symbols')[0].checked;
 
+    $('#result').text(generatePassword(length, upper, lower, numbers, symbols));
+});
+
+const generatePassword = (
+    length,
+    upper,
+    lower,
+    numbers,
+    symbols
+) => {
+
+    console.log(length, upper, lower, numbers, symbols);
+
+    return 'hello';
+};
 
 // character codes - https://net-comber.com/charset.html
-const getRandomLowercase = () => String.fromCharCode(Math.floor(Math.random() * 26 + 97));
 const getRandomUppercase = () => String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+const getRandomLowercase = () => String.fromCharCode(Math.floor(Math.random() * 26 + 97));
 const getRandomNumber = () => JSON.stringify(Math.floor(Math.random() * 10));
 const getRandomSymbol = () => {
     const symbols = '!@#$%^&*()-_=+[{]}<>?';
     return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-console.log(getRandomLowercase());
-console.log(getRandomUppercase());
-console.log(getRandomNumber());
-console.log(getRandomSymbol());
-
+};
